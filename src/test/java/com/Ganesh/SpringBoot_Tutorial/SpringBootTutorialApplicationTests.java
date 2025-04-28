e2f5daac49ed4bf938d8fb6048a68c282ac063e1
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-04-28 13:32:44 +0530

    uploaded unversioned files

---
+++ src/test/java/com/Ganesh/SpringBoot_Tutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ganesh.SpringBoot_Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
