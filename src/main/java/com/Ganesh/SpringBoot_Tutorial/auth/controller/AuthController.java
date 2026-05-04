e45b74dbc1764d1b8e2089299f8208f0d1781537
Ganesh Renuse <ganesh21renuse3@gmail.com>
2026-05-05 00:17:32 +0530

    Updated Files.

---
+++ src/main/java/com/Ganesh/SpringBoot_Tutorial/auth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.Ganesh.SpringBoot_Tutorial.auth.controller;
+
+import com.Ganesh.SpringBoot_Tutorial.auth.dto.AuthRequest;
+import com.Ganesh.SpringBoot_Tutorial.auth.dto.AuthResponse;
+import com.Ganesh.SpringBoot_Tutorial.auth.util.JwtUtil;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+
+        // TEMPORARY (we'll improve later)
+        if ("admin".equals(request.getUsername()) &&
+                "password".equals(request.getPassword())) {
+
+            String token = JwtUtil.generateToken(request.getUsername());
+            return new AuthResponse(token);
+        }
+
+        throw new RuntimeException("Invalid credentials");
+    }
+}
---
+++ src/main/java/com/Ganesh/SpringBoot_Tutorial/auth/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.Ganesh.SpringBoot_Tutorial.auth.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET = "mysecretkeymysecretkeymysecretkey";
+
+    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
